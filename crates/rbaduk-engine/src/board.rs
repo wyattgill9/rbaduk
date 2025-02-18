8e2ca7f45a4fd36966f51cccd17781b0a8f7e235
Shidowy <101477459+Shidowy@users.noreply.github.com>
2025-02-17 16:53:07 -0800

    Chai T. Rex's Opts

--- crates/rbaduk-engine/src/board.rs
+++ crates/rbaduk-engine/src/board.rs
@@ -1,46 +1,90 @@
-use std::mem;
+use core::mem;
 
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[repr(u8)]
 pub enum Stone {
+    #[default]
     Empty,
     Black,
     White,
 }
 
+impl core::fmt::Display for Stone {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_str(match self {
+            Self::Empty => ".",
+            Self::Black => "B",
+            Self::White => "W",
+        })
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Board {
-    data: Vec<u8>,
+    board: Vec<u8>,
 }
 
 impl Board {
     pub fn new() -> Self {
-        Board {
-            data: {
-                let data = vec![0b00000000; 91];
-                data
-            },
+        const EMPTY_FILL: u8 = Stone::Empty as u8 * 0b00000000;
+        Self {
+            board: vec![EMPTY_FILL; 91],
         }
     }
 
-    pub fn place_stone(&mut self, pos: (u8, u8), stone: Stone) {
-        let index = (pos.0 * 19 + pos.1) as usize;
-        let u8_index = index / 4;
-        let bit_offset = (index % 4) * 2;
+    pub fn get(&self, row: usize, col: usize) -> Stone {
+        if row >= 19 {
+            panic!("`row` must be less than 19, but is {row}");
+        }
+        if col >= 19 {
+            panic!("`col` must be less than 19, but is {col}");
+        }
 
-        let stone_value = match stone {
-            Stone::Empty => 0b00,
-            Stone::Black => 0b01,
-            Stone::White => 0b10,
-        };
+        // SAFETY: Both `row` and `col` are less than 19.
+        unsafe { self.get_unchecked(row, col) }
+    }
 
-        // mask to clear the existing 2 bits at the pos
-        let mask = !(0b11 << bit_offset);
+    /// # Safety
+    /// Both `row` and `col` must be less than 19.
+    pub unsafe fn get_unchecked(&self, row: usize, col: usize) -> Stone {
+        let bit_index = 2 * (19 * row + col);
+        // SAFETY: If both `row` and `col` are less than 19, `bit_index / 8`
+        // will be a valid index.
+        let stone_code =
+            (unsafe { self.board.get_unchecked(bit_index / 8) } >> (bit_index % 8)) & 0b11;
+        // SAFETY: `Stone` is represented as a `u8`, so transmutation is safe as
+        // long as the `u8` here is a `Stone` discriminant. The `u8` here is a
+        // `Stone` discriminant, as we don't allow invalid bit patterns into
+        // `Board.board`.
+        unsafe { core::mem::transmute(stone_code) }
+    }
 
-        //clear the existing bits and set new stone value
-        self.data[u8_index] &= mask;
-        self.data[u8_index] |= stone_value << bit_offset;
+    pub fn set(&mut self, row: usize, col: usize, stone: Stone) {
+        if row >= 19 {
+            panic!("`row` must be less than 19, but is {row}");
+        }
+        if col >= 19 {
+            panic!("`col` must be less than 19, but is {col}");
+        }
+
+        // SAFETY: Both `row` and `col` are less than 19.
+        unsafe { self.set_unchecked(row, col, stone) }
+    }
+
+    /// # Safety
+    /// Both `row` and `col` must be less than 19.
+    pub unsafe fn set_unchecked(&mut self, row: usize, col: usize, stone: Stone) {
+        let bit_index = 2 * (19 * row + col);
+        // SAFETY: If both `row` and `col` are less than 19, `bit_index / 8`
+        // will be a valid index.
+        let entry = unsafe { self.board.get_unchecked_mut(bit_index / 8) };
+        let shift = bit_index % 8;
+        *entry &= !(0b11 << shift);
+        *entry |= (stone as u8) << shift;
     }
 
     pub fn get_board_mem(&self) {
-        let data = &self.data;
+        let data = &self.board;
         println!(
             "Memory size of data array: {} bytes",
             mem::size_of_val(data)
@@ -52,25 +96,19 @@ impl Board {
     }
 }
 
-impl std::fmt::Display for Board {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl Default for Board {
+    fn default() -> Self {
+        Board::new()
+    }
+}
+
+impl core::fmt::Display for Board {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         for row in 0..19 {
             for col in 0..19 {
-                let index = (row * 19 + col) as usize;
-                let u8_index = index / 4;
-                let bit_offset = (index % 4) * 2;
-
-                let stone_value = (self.data[u8_index] >> bit_offset) & 0b11;
-
-                let display_char = match stone_value {
-                    0b01 => 'B',
-                    0b10 => 'W',
-                    _ => '.',
-                };
-
-                write!(f, "{} ", display_char)?;
+                self.get(row, col).fmt(f)?;
             }
-            writeln!(f)?;
+            f.write_str("\n")?;
         }
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use engine::{
-    board,
-    board::Stone::{Black, White},
+    board::Board,
+    board::Stone,
 };
 
 fn main() {
-    let mut board = board::Board::new();
+    let mut board = Board::new();
+    println!("{board}");
 
-    board.place_stone((0, 0), Black);
-    board.place_stone((1, 1), White);
-
-    println!("{}", board);
-    board.get_board_mem();
+    // Put pieces on the board with a repeating ".BW" pattern to check that
+    // index computations are correct.
+    let mut iter = [Stone::Empty, Stone::Black, Stone::White]
+        .into_iter()
+        .cycle();
+    for row in 0..19 {
+        for col in 0..19 {
+            board.set(row, col, iter.next().unwrap());
+        }
+    }
+    println!("{board}");
 }
